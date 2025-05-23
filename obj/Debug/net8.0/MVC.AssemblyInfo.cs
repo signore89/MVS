7ca1da2d87acba282f64593aa0f2ad5257cb89fe
Dima <derferth@yandex.ru>
2025-05-23 21:50:16 +0500

    добавление страницы "О нас". Завершение проекта

--- obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e161dcd09eb618bc555cbc0d14c11e1e0973a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97fe0d53513dad1219018fb73a558ffe9b1cd97")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
